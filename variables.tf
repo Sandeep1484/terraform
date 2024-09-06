ca6f13e6122fb2580acc0e7c9898df27cc546b79
Sandeep1484 <167241240+Sandeep1484@users.noreply.github.com>
2024-09-06 16:03:16 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "ami_value" {
+    description = "value for the ami"
+  
+}
+
+variable "instance_type_value" {
+    description = "value for the instance_type"
+    type = string
+  
+}
+
+
+variable "key_name_value" {
+    description = "value for the key_name"
+  
+}
+
+variable "subnet_id_value" {
+    description = "value for sub net id"
+  
+}
