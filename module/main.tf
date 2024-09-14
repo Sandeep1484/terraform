aacf9e9c68a9dcada744204c2fbd8b192e69c91f
Sandeep1484 <167241240+Sandeep1484@users.noreply.github.com>
2024-09-14 11:28:49 +0530

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-    region = "us-east-2"
+  region = "us-east-1"
 }
 
-resource "aws_instance" "demo" {
-    ami = var.ami_value
-    instance_type = var.instance_type_value
-    key_name = var.key_name_value
-    subnet_id = var.subnet_id_value
+module "ec2_instance" {
+  source = "./modules/ec2_instance"
+  ami_value = "ami-053b0d53c279acc90" # replace this
+  instance_type_value = "t2.micro"
+  subnet_id_value = "subnet-019ea91ed9b5252e7". # replace this
 }
